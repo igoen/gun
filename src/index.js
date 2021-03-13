09099c3af53564d5c16d8a4be5fed9f146ff6590
igoen <me@igoen.com>
2021-03-13 16:28:32 +0700

    adding

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var Swandono = /** @class */ (function () {
+    function Swandono() {
+    }
+    Swandono.prototype.hi = function () {
+        console.log('Hello World!');
+    };
+    return Swandono;
+}());
